82c17c68aa268da9326326c82ef90b1d7040e0df
Ayhem18 <bouabidayhem@gmail.com>
2022-04-06 00:02:17 +0300

    stage 2 validated

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from multipledispatch import dispatch
 import pandas as pd
 
 pd.set_option('display.max_columns', 8)
@@ -7,14 +6,21 @@ general = 'test/general.csv'
 prenatal = 'test/prenatal.csv'
 sports = 'test/sports.csv'
 lines_display = 20
+random_state_value = 30
 
 if __name__ == '__main__':
     general_df = pd.read_csv(general)
     prenatal_df = pd.read_csv(prenatal)
     sports_df = pd.read_csv(sports)
 
-    print(general_df.head(lines_display))
+    # the column names in all dataframes should match
+    prenatal_df.columns = general_df.columns
+    sports_df.columns = general_df.columns
 
-    print(prenatal_df.head(lines_display))
+    # merge all the dataframes into one large dataframe
+    hospitals = pd.concat([general_df, prenatal_df, sports_df], ignore_index=True)
 
-    print(sports_df.head(lines_display))
+    # delete the first column '
+    hospitals.drop(columns=hospitals.columns[0], inplace=True)
+
+    print(hospitals.sample(n=lines_display, random_state=random_state_value))
