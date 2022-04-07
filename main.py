ade09884878af7fa657286f141873ef1ad6f8bfc
Ayhem18 <bouabidayhem@gmail.com>
2022-04-07 15:00:21 +0300

    stage3 validated

--- main.py
+++ main.py
@@ -21,11 +21,39 @@ def stage2_implementation():
     # merge all the dataframes into one large dataframe
     hospitals = pd.concat([general_df, prenatal_df, sports_df], ignore_index=True)
 
-    # delete the first column '
+    # delete the first column
     hospitals.drop(columns=hospitals.columns[0], inplace=True)
+
+    return hospitals
+
+
+gender_mapper = {'female': 'f', 'male': 'm', 'woman': 'f', 'man': 'm'}
+gender = 'gender'
+hospital_type = 'hospital'
+zero_columns = ['bmi', 'diagnosis', 'blood_test', 'ecg', 'ultrasound', 'mri', 'xray', 'children', 'months']
+
+
+def stage3_implementation():
+    hospitals = stage2_implementation()
+    # delete all the empty rows
+    hospitals.dropna(axis='index', how='all', inplace=True)
+
+    # unify the genders
+    hospitals.loc[:, gender] = [gender_mapper[g] if g in gender_mapper else g for g in hospitals[gender]]
+
+    # replace the Nan values with f in the prenatal hospital
+    hospitals[gender] = hospitals.groupby(hospital_type)[gender].apply(lambda x: x.fillna('f'))
+
+    # print(hospitals.loc[hospitals.hospital == 'prenatal', ['hospital', 'gender']])
+    # print("####################################")
+    # replace the Nan values with 0 in the corresponding columns
+    hospitals[zero_columns] = hospitals[zero_columns].fillna(0)
+
+    # print the shape of the dataframe
+    print(hospitals.shape)
 
     print(hospitals.sample(n=lines_display, random_state=random_state_value))
 
 
 if __name__ == '__main__':
-    pass
+    stage3_implementation()
