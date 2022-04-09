12f13c30152b7a473e2c32f00c32f9db7ac546be
Ayhem18 <bouabidayhem@gmail.com>
2022-04-09 12:15:34 +0300

    stage 4 validated

--- main.py
+++ main.py
@@ -52,19 +52,24 @@ def stage3_implementation():
     # print the shape of the dataframe
     # print(hospitals.shape)
 
-    # print(hospitals)
+    # print(hospitals.head(10))
     return hospitals
 
 
 stomach = 'stomach'
 dislocation = 'dislocation'
+question1 = 'The answer to the 1st question is {}'
+question2 = 'The answer to the 2nd question is {}'
+question3 = 'The answer to the 3rd question is {}'
+question4 = 'The answer to the 4th question is {}'
+question5 = 'The answer to the 5th question is {}, {} blood tests'
 
 
 def stage4_implementation():
     hospitals = stage3_implementation()
 
     # determine the hospital with the largest number of patients
-    # print(hospitals[hospital_type].mode()[0])
+    print(question1.format(str(hospitals[hospital_type].mode()[0])))
 
     # determine the share of patients in the general hospital that suffer from stomach related issues
 
@@ -72,22 +77,29 @@ def stage4_implementation():
     general_patients_stomach = \
         hospitals.loc[(hospitals.diagnosis == stomach) & (hospitals.hospital == 'general'), :].shape[0]
 
-    print(round(general_patients_stomach / general_patients, 3))
+    print(question2.format(str(round(general_patients_stomach / general_patients, 3))))
 
     # determine the share of patients in the sports hospital that suffer from dislocation-related issues
 
-    sports_patients = hospitals.loc[
-                      (hospitals.hospital == 'sports') & (hospitals.diagnosis == dislocation), :].shape[0]
+    sports_patients = hospitals.loc[hospitals.hospital == 'sports', :].shape[0]
 
     sports_patients_dislocation = \
-        hospitals.loc[hospitals.diagnosis == dislocation, :].shape[0]
+        hospitals.loc[(hospitals.hospital == 'sports') & (hospitals.diagnosis == dislocation), :].shape[0]
 
-    print(round(sports_patients_dislocation / sports_patients, 3))
+    print(question3.format(str(round(sports_patients_dislocation / sports_patients, 3))))
 
     # find the difference in median ages of the patient in general and the one in sports
     new_table = pd.pivot_table(hospitals, index='hospital', values='age', aggfunc='median')
-    print(new_table)
-    print(new_table.loc['general', 'age'] - new_table.loc['sports', 'age'])
+    # print(new_table)
+    print(question4.format(str(
+        new_table.loc[['general'], ['age']].age.median() - new_table.loc[['sports'], ['age']].age.median())))
+
+    # find the hospital with the largest number of blood tests
+    blood_test_table = pd.pivot_table(hospitals.loc[hospitals.blood_test == 't', :],
+                                      index='hospital', values='blood_test', aggfunc='count')
+    max_blood_test = blood_test_table.blood_test.max()
+    print(question5.format(str(blood_test_table.index[blood_test_table['blood_test'] == max_blood_test][0]),
+                           str(max_blood_test)))
 
 
 if __name__ == '__main__':
